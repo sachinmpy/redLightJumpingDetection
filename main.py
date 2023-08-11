479a762937ccef52dd3d2b54f2cb9f5298dd75b4
sachinmpy <smathuria18@gmail.com>
2023-08-11 22:40:18 +0530

    refactor code

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import logging
 import settings
-import datetime
-import RLJDGui as GUI
 
 from databaseHandler import RljdDBHandler
 
 logging.basicConfig(level=logging.DEBUG, format=" [ %(levelname)s ] - %(name)s - %(message)s", )
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     db: RljdDBHandler = RljdDBHandler(settings.DBSettings, 'infringement')
+
     logger.info("Starting..")
